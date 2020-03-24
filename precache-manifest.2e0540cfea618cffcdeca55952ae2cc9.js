d05418f429a4d66003b34c5a446206da34b51bac
Ann <oxid303@gmail.com>
2020-03-25 01:02:19 +0300

    Updates

---
+++ precache-manifest.2e0540cfea618cffcdeca55952ae2cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308ce4f548e6ebb6f20dfede6c3e2a93",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "2ebe8fc84f1f2675a8c2",
+    "url": "/todolist/static/css/main.a0c14557.chunk.css"
+  },
+  {
+    "revision": "0fe7c64cc61934bfe86e",
+    "url": "/todolist/static/js/2.7f7cbb1c.chunk.js"
+  },
+  {
+    "revision": "9adccee2fe9e6e7815c0f1049414ca41",
+    "url": "/todolist/static/js/2.7f7cbb1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebe8fc84f1f2675a8c2",
+    "url": "/todolist/static/js/main.51a38441.chunk.js"
+  },
+  {
+    "revision": "cebccde0f7f76a903cc9",
+    "url": "/todolist/static/js/runtime-main.6fa7f924.js"
+  }
+]);
